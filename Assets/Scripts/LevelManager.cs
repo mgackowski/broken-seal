838dc0e6941f9824392cd49ba665d125568b93d1
Georgi Tsaklev <georgi.tsaklev@gmail.com>
2022-01-29 16:15:21 +0000

    LevelManager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+  private static LevelManager instance;
+
+  private int score = 0;
+  [SerializeField]
+  private int lives = 3;
+  [SerializeField]
+  private Vector3 playableAreaSize = new Vector3(10, 10, 10);
+  private Bounds playableArea;
+
+  private void Start() {
+    instance = this;
+    playableArea = new Bounds(Vector3.zero, instance.playableAreaSize);
+  }
+
+  public static Bounds PlayableArea
+  {
+    get
+    {
+      return instance.playableArea;
+    }
+  }
+
+  public static void LoseLive() {
+    instance.lives -= 1;
+    if (instance.lives < 1) {
+      // TODO: Lose screen
+      // TODO: Reload the scene
+    }
+  }
+}
